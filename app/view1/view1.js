65eea4557ae18c9b5f6d5db8328853cf07c0d2d6
Ashish Gaurav <ashish@archsaber.com>
2016-08-11 16:30:54 +0530

    init

---
+++ app/view1/view1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute', 'angular-flatpickr'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+    });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.dateOpts = {
+        dateFormat: 'Y-m-d',
+        defaultDate: '2016-03-01 03:30:00 -0300'
+    };
+
+    $scope.datePostSetup = function(fpItem) {
+        console.log('flatpickr', fpItem);
+    }
+}]);
